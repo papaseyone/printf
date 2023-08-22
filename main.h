5251205578fc5b7b852e7ce1a4ef174e7646b1b6
papaseyone <charleskumahntim@gmail.com>
2023-08-22 08:28:46 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef main_h
+#define main_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+
+int _putchar(char c);
+int _puts(char *c);
+int _printf(const char *format, ...);
+
+#endif
